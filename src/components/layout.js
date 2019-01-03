e0b42731743bc534aa537c6d777ebd5462a6d461
maecapozzi <maecapozzi@gmail.com>
2019-01-03 07:29:28 -0500

    Upgrade to Gatsby v2

---
+++ src/components/layout.js
@@ -0,0 +1,84 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+
+import "../layouts/index.css";
+
+const Header = () => (
+  <div
+    style={{
+      background: "#272B2E",
+      marginBottom: "1.45rem",
+      borderBottom: "5px solid pink",
+      zIndex: "1"
+    }}
+  >
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "1.45rem 1.0875rem"
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: "#FFBAC8",
+            textDecoration: "none"
+          }}
+        >
+          Mae Capozzi
+        </Link>
+      </h1>
+
+      <Link
+        style={{
+          color: "#FFBAC8",
+          textDecoration: "none"
+        }}
+        to="/about"
+      >
+        About
+      </Link>
+    </div>
+  </div>
+);
+
+const Layout = ({ children }) => (
+  <div>
+    <Helmet
+      title="Mae Capozzi"
+      meta={[
+        {
+          name: "description",
+          content:
+            "Mae Capozzi writes about web development and software engineering ethics."
+        },
+        {
+          name: "keywords",
+          content:
+            "reactjs, software, coding, technology, web development, ethics"
+        }
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
+      }}
+    >
+      {children}
+    </div>
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.func
+};
+
+export default Layout;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,394 +1,397 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/layout";
 
 const AboutPage = () => (
-  <div
-    style={{
-      position: 'absolute',
-      top: '110',
-      left: '0',
-      bottom: '0',
-      right: '0',
-      height: '500vh',
-      width: '100%',
-      textAlign: 'center',
-      backgroundColor: '#272B2D',
-      color: 'pink',
-      fontFamily: 'Inconsolata, monospace',
-      zIndex: '0'
-    }}
-  >
-    <section
+  <Layout>
+    <div
       style={{
-        padding: '20px',
-        margin: '10px'
+        position: "absolute",
+        top: "110",
+        left: "0",
+        bottom: "0",
+        right: "0",
+        height: "500vh",
+        width: "100%",
+        textAlign: "center",
+        backgroundColor: "#272B2D",
+        color: "pink",
+        fontFamily: "Inconsolata, monospace",
+        zIndex: "0"
       }}
     >
-      <h1 style={{ fontSize: '75px' }}>Mae Capozzi</h1>
-    </section>
-    <section
-      style={{
-        padding: '20px',
-        margin: '10px'
-      }}
-    >
-      <h2
-        style={{
-          fontFamily: 'Inconsolata, monospace'
-        }}
-      >
-        Software Engineer
-      </h2>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://github.com/maecapozzi'
-        >
-          Github
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://twitter.com/MCapoz'
-        >
-          Twitter
-        </a>
-      </p>
-    </section>
-    <div>
-      <div>
-        <section
-          style={{
-            padding: '20px',
-            margin: '10px'
-          }}
-        >
-          <p
-            style={{
-              color: 'pink',
-              fontFamily: 'Inconsolata, monospace'
-            }}
-          >
-            Previously: 501 Auctions, Thrive Global
-          </p>
-          <p
-            style={{
-              color: 'pink',
-              fontFamily: 'Inconsolata, monospace'
-            }}
-          >
-            Currently: InRhythm
-          </p>
-        </section>
-      </div>
-    </div>
-    <div>
       <section
         style={{
-          padding: '20px',
-          margin: '10px'
+          padding: "20px",
+          margin: "10px"
+        }}
+      >
+        <h1 style={{ fontSize: "75px" }}>Mae Capozzi</h1>
+      </section>
+      <section
+        style={{
+          padding: "20px",
+          margin: "10px"
         }}
       >
         <h2
           style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
+            fontFamily: "Inconsolata, monospace"
           }}
         >
-          Technologies
+          Software Engineer
         </h2>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          ES6
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://github.com/maecapozzi"
+          >
+            Github
+          </a>
         </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          React.js
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          Node.js
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          CSS
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          Sass
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          styled-components
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          css-modules
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          webpack
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          git
-        </p>
-        <p
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-        >
-          Github
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://twitter.com/MCapoz"
+          >
+            Twitter
+          </a>
         </p>
       </section>
+      <div>
+        <div>
+          <section
+            style={{
+              padding: "20px",
+              margin: "10px"
+            }}
+          >
+            <p
+              style={{
+                color: "pink",
+                fontFamily: "Inconsolata, monospace"
+              }}
+            >
+              Previously: 501 Auctions, Thrive Global
+            </p>
+            <p
+              style={{
+                color: "pink",
+                fontFamily: "Inconsolata, monospace"
+              }}
+            >
+              Currently: InRhythm
+            </p>
+          </section>
+        </div>
+      </div>
+      <div>
+        <section
+          style={{
+            padding: "20px",
+            margin: "10px"
+          }}
+        >
+          <h2
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            Technologies
+          </h2>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            ES6
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            React.js
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            Node.js
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            CSS
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            Sass
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            styled-components
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            css-modules
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            webpack
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            git
+          </p>
+          <p
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+          >
+            Github
+          </p>
+        </section>
+      </div>
+
+      <section
+        style={{
+          padding: "20px",
+          margin: "10px"
+        }}
+      >
+        <h2>Side Projects</h2>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="http://www.ear-worm.com/"
+          >
+            earworm
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://www.npmjs.com/package/react-scroll-activator"
+          >
+            react-scroll-activator
+          </a>
+        </p>
+      </section>
+
+      <section
+        style={{
+          padding: "20px",
+          margin: "10px"
+        }}
+      >
+        <h2>Posts</h2>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://code.likeagirl.io/simplified-javascript-getting-started-with-closures-f40f65317d00"
+          >
+            Simplified JavaScript: Getting Started with Closures
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://hackernoon.com/simplified-javascript-hoisting-9128b74bb397"
+          >
+            Simplified JavaScript: Hoisting
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://hackernoon.com/simplified-javascript-how-do-i-use-var-let-and-const-a504be4fb86f"
+          >
+            Simplified JavaScript: How do I use var, let, and const?
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://code.likeagirl.io/how-to-set-up-d3-js-with-webpack-and-babel-7bd3f5e20df7"
+          >
+            How to Set Up D3.js with Webpack and Babel
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-1-f7afa3cd0cc8"
+          >
+            Tutorial: How to make HTTP requests in React, Part 1
+          </a>
+        </p>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-2-4cfdba3ec65"
+          >
+            Tutorial: How to make HTTP requests in React, Part 2
+          </a>
+        </p>
+        <p>
+          <a href="https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-3-daa6b31b66be">
+            Tutorial: How to make HTTP requests in React, Part 3
+          </a>
+        </p>
+      </section>
+
+      <section
+        style={{
+          padding: "20px",
+          margin: "10px"
+        }}
+      >
+        <h2>What I'm Learning and Building</h2>
+        <ul>
+          *
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://github.com/maecapozzi/spotify-analyzer-frontend/pull/19"
+          >
+            Refactoring my side project into redux
+          </a>
+        </ul>
+        <ul>
+          *
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://github.com/maecapozzi/spotify-analyzer-frontend/pull/15"
+          >
+            Upgrading my side project to React 16
+          </a>
+        </ul>
+        <ul>
+          *
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://github.com/maecapozzi/spotify-node-express/pull/11"
+          >
+            Learning about databases with MySQL and knex
+          </a>
+        </ul>
+        <ul>
+          *
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://twitter.com/MCapoz/status/936629455484850177"
+          >
+            Writing thoughtful and expressive pull request descriptions
+          </a>
+        </ul>
+        <ul>
+          *
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://github.com/maecapozzi/spotify-analyzer-frontend/pull/12"
+          >
+            Testing my React code with jest and enzyme
+          </a>
+        </ul>
+      </section>
+
+      <section
+        style={{
+          padding: "20px",
+          margin: "10px"
+        }}
+      >
+        <h2>Interests</h2>
+        <p>
+          <a
+            style={{
+              color: "pink",
+              fontFamily: "Inconsolata, monospace"
+            }}
+            href="https://soundcloud.com/maecapozzi"
+          >
+            Music
+          </a>
+        </p>
+        <p>English Literature</p>
+        <p>Italian</p>
+        <p>Product Management</p>
+      </section>
     </div>
+  </Layout>
+);
 
-    <section
-      style={{
-        padding: '20px',
-        margin: '10px'
-      }}
-    >
-      <h2>Side Projects</h2>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='http://www.ear-worm.com/'
-        >
-          earworm
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://www.npmjs.com/package/react-scroll-activator'
-        >
-          react-scroll-activator
-        </a>
-      </p>
-    </section>
-
-    <section
-      style={{
-        padding: '20px',
-        margin: '10px'
-      }}
-    >
-      <h2>Posts</h2>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://code.likeagirl.io/simplified-javascript-getting-started-with-closures-f40f65317d00'
-        >
-          Simplified JavaScript: Getting Started with Closures
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://hackernoon.com/simplified-javascript-hoisting-9128b74bb397'
-        >
-          Simplified JavaScript: Hoisting
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://hackernoon.com/simplified-javascript-how-do-i-use-var-let-and-const-a504be4fb86f'
-        >
-          Simplified JavaScript: How do I use var, let, and const?
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://code.likeagirl.io/how-to-set-up-d3-js-with-webpack-and-babel-7bd3f5e20df7'
-        >
-          How to Set Up D3.js with Webpack and Babel
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-1-f7afa3cd0cc8'
-        >
-          Tutorial: How to make HTTP requests in React, Part 1
-        </a>
-      </p>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-2-4cfdba3ec65'
-        >
-          Tutorial: How to make HTTP requests in React, Part 2
-        </a>
-      </p>
-      <p>
-        <a href='https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-3-daa6b31b66be'>
-          Tutorial: How to make HTTP requests in React, Part 3
-        </a>
-      </p>
-    </section>
-
-    <section
-      style={{
-        padding: '20px',
-        margin: '10px'
-      }}
-    >
-      <h2>What I'm Learning and Building</h2>
-      <ul>
-        *
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://github.com/maecapozzi/spotify-analyzer-frontend/pull/19'
-        >
-          Refactoring my side project into redux
-        </a>
-      </ul>
-      <ul>
-        *
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://github.com/maecapozzi/spotify-analyzer-frontend/pull/15'
-        >
-          Upgrading my side project to React 16
-        </a>
-      </ul>
-      <ul>
-        *
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://github.com/maecapozzi/spotify-node-express/pull/11'
-        >
-          Learning about databases with MySQL and knex
-        </a>
-      </ul>
-      <ul>
-        *
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://twitter.com/MCapoz/status/936629455484850177'
-        >
-          Writing thoughtful and expressive pull request descriptions
-        </a>
-      </ul>
-      <ul>
-        *
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://github.com/maecapozzi/spotify-analyzer-frontend/pull/12'
-        >
-          Testing my React code with jest and enzyme
-        </a>
-      </ul>
-    </section>
-
-    <section
-      style={{
-        padding: '20px',
-        margin: '10px'
-      }}
-    >
-      <h2>Interests</h2>
-      <p>
-        <a
-          style={{
-            color: 'pink',
-            fontFamily: 'Inconsolata, monospace'
-          }}
-          href='https://soundcloud.com/maecapozzi'
-        >
-          Music
-        </a>
-      </p>
-      <p>English Literature</p>
-      <p>Italian</p>
-      <p>Product Management</p>
-    </section>
-  </div>
-)
-
-export default AboutPage
+export default AboutPage;
