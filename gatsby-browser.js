3b4a7030202a5bb935c0afe54530629e55c6df41
maecapozzi <maecapozzi@gmail.com>
2020-01-23 14:32:42 -0500

    Upgrade dependencies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,10 +5,6 @@ import "./src/styles/global.css";
 // custom typefaces
 import "typeface-inconsolata";
 import "typeface-nunito";
-import "typeface-montserrat";
-import "typeface-roboto";
-import "typeface-major-mono-display";
-import "typeface-quicksand";
 
 export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
