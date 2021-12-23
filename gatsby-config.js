f233e7e1bbc184a0cb1a6489880d6401d7f92a6d
Mae Capozzi <maecapozzi@gmail.com>
2021-12-23 18:51:45 -0500

    Add temporary christmas drinks menu

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/christmas2021`,
+        name: `christmas2021`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
